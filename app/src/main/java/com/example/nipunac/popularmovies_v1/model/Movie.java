925e27b7eb38d22061c3f24cd257aba9251f45d9
NipunaCooray <nipuna.cooray@gmail.com>
2018-08-07 19:19:09 +1000

    MVP need to impl click

--- app/src/main/java/com/example/nipunac/popularmovies_v1/model/Movie.java
+++ app/src/main/java/com/example/nipunac/popularmovies_v1/model/Movie.java
@@ -4,14 +4,58 @@ package com.example.nipunac.popularmovies_v1.model;
 
 public class Movie  {
 
-    private String poster;
+    private String posterURL;
     private String originalTitle;
     private String moviePosterThumbnail;
     private String plotSynopsis;
     private int userRating;
     private String releaseDate;
 
+    public String getPosterURL() {
+        return posterURL;
+    }
 
+    public void setPosterURL(String posterURL) {
+        this.posterURL = posterURL;
+    }
 
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
 
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getMoviePosterThumbnail() {
+        return moviePosterThumbnail;
+    }
+
+    public void setMoviePosterThumbnail(String moviePosterThumbnail) {
+        this.moviePosterThumbnail = moviePosterThumbnail;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+
+    public void setPlotSynopsis(String plotSynopsis) {
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    public int getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(int userRating) {
+        this.userRating = userRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
