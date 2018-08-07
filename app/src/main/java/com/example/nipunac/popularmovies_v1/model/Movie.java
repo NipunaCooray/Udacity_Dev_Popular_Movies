faf09e6a557d66c91bb551749e2937f99644c3e1
NipunaCooray <nipuna.cooray@gmail.com>
2018-08-07 17:07:19 +1000

    Changed gradele

---
+++ app/src/main/java/com/example/nipunac/popularmovies_v1/model/Movie.java
@@ -0,0 +1,17 @@
+package com.example.nipunac.popularmovies_v1.model;
+
+
+
+public class Movie  {
+
+    private String poster;
+    private String originalTitle;
+    private String moviePosterThumbnail;
+    private String plotSynopsis;
+    private int userRating;
+    private String releaseDate;
+
+
+
+
+}
--- app/src/main/java/com/example/nipunac/popularmovies_v1/utilities/TheMovieDBJSonUtils.java
+++ app/src/main/java/com/example/nipunac/popularmovies_v1/utilities/TheMovieDBJSonUtils.java
@@ -1,7 +1,17 @@
 package com.example.nipunac.popularmovies_v1.utilities;
 
+import android.content.Context;
+
+import org.json.JSONException;
+
 public final class TheMovieDBJSonUtils {
 
+    public static String[] getSimpleWeatherStringsFromJson(Context context, String movieListJsonStr)
+            throws JSONException {
 
+
+
+        return null;
+    }
 
 }
